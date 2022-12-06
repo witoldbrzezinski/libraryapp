105ea1f15cc7040d2ddaf55bf1e44ef5566dbb36
Witold Brzezinski <witbrzezinski@gmail.com>
2022-12-06 13:12:30 +0100

    spring initializr files

---
+++ src/test/java/pl/witoldbrzezinski/libraryapp/LibraryappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.witoldbrzezinski.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
