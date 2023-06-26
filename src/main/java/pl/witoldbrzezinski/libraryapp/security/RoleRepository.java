ac8c30e5df60f58be9f7a9592ddb619badc1877b
witoldbrzezinski <witbrzezinski@gmail.com>
2023-06-26 14:30:31 +0200

    added security config and jwt

---
+++ src/main/java/pl/witoldbrzezinski/libraryapp/security/RoleRepository.java
@@ -0,0 +1,7 @@
+package pl.witoldbrzezinski.libraryapp.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {}
---
+++ src/main/java/pl/witoldbrzezinski/libraryapp/security/UserRepository.java
@@ -0,0 +1,9 @@
+package pl.witoldbrzezinski.libraryapp.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+  Optional<UserEntity> findByUsername(String username);
+}
